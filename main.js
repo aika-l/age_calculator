e4dc33dc9a67cd9b52ed0edd617cfcf32c4441dd
a20l <aidaytyn@gmail.com>
2021-03-02 15:54:49 -0600

    created a file that calculates the possible age

---
+++ main.js
@@ -0,0 +1,7 @@
+var current_year = 2021;
+var birth_year = 2016;
+
+var age = current_year - birth_year;
+var possible_age = age - 1;
+
+document.write("You are either " + possible_age + " or  " + age);
